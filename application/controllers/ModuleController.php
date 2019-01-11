b05b179c219f969be80ff4c65f2209f4226671e7
Jacob Bakarich <jacob.bakarich@gmail.com>
2019-01-11 10:49:53 -0700

    changed name to match repo

--- application/controllers/ModuleController.php
+++ application/controllers/ModuleController.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace Icinga\Module\import_plugin\Controllers;
+namespace Icinga\Module\director_icinga2-director-import-plugin\Controllers;
 use Icinga\Web\Controller;
 
 class ModuleController extends Controller{
